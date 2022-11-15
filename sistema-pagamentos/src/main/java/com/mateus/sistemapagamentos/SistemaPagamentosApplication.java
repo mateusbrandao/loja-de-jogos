87090d0141672bda2d382aa8b26f46327c92119f
Mateus <mateusbrandao@id.uff.br>
2022-11-15 17:51:47 -0300

    Criacao do projeto sistema-pagamentos

---
+++ sistema-pagamentos/src/main/java/com/mateus/sistemapagamentos/SistemaPagamentosApplication.java
@@ -0,0 +1,13 @@
+package com.mateus.sistemapagamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaPagamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaPagamentosApplication.class, args);
+	}
+
+}
---
+++ sistema-pagamentos/src/test/java/com/mateus/sistemapagamentos/SistemaPagamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateus.sistemapagamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaPagamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
